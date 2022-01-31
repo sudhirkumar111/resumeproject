43b0f15f533c19854284d4cf56c0a3e486b66fce
sudhir <kumar9king@gmail.com>
2022-02-01 03:08:37 +0530

    second commit

---
+++ procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT resumeproject.wsgi:application
--- resumeproject/settings.py
+++ resumeproject/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vs=^8fw$xb84(p8ail_r_++&(r--ccn*cgr^40pa2x=y&e7goh'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
